846f37033dca5635bafb741fc4c7a3c6d45bb212
Kampala <ezeanyakampala@gmail.com>
2023-10-18 12:54:41 +0100

    commit update

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const paypal = require('paypal-rest-sdk');
+const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
+const DB_URL = process.env.MONGO_URL;
+
+paypal.configure({
+  mode: 'sandbox',
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET
+});
+
+mongoose
+    .connect(DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'paygate'
+    })
+    .then(() => {
+        console.log('database connection successful');
+    })
+    .catch((err) => {
+        console.error('database connection error', err);
+    });
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
---
+++ models/card.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+    number: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    expire_month: {
+        type: String,
+        required: true
+    },
+    expire_year: {
+        type: String,
+        required: true
+    },
+    cvv2: {
+        type: String,
+        required: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
